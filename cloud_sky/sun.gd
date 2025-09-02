f3aaf9dc44f9855baf72ec390c235e4fd2197d1a
clayjohn <claynjohn@gmail.com>
2025-09-01 21:10:38 -0700

    Extract cleanup into a function that can be called to ensure we are not leaking GPU memory. It is also responsible for resetting state so we can update settings at run time.
    Also, defer assigning the sun to ensure it doesn't get set until after the cloud_sky resource is ready

--- cloud_sky/sun.gd
+++ cloud_sky/sun.gd
@@ -5,6 +5,10 @@ var atmosphere_tex := load(get_script().resource_path.get_base_dir() + "/sky_lut
 var cloud_sky := load(get_script().resource_path.get_base_dir() + "/clouds_sky.tres")
 
 func _ready():
+	call_deferred("delayed_init")
+	
+# Needs to be called after cloud_sky is fully initialized
+func delayed_init():
 	cloud_sky.sun = self
 	cloud_sky.request_full_sky_init()
 	
